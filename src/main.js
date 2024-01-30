7167bd83d96076ce835176c0b136d1f78a097e34
Vitalii Chyhirov <vitalii@galaxia.bio>
2024-01-30 14:08:49 +0100

    testing: tree-structures: forward diagram

--- src/main.js
+++ src/main.js
@@ -1,3 +1,214 @@
+// 
+
+class Location {
+    #data
+    constructor(data) {
+        this.#data = data
+    }
+
+    equals(other) {
+        if (this.#data.length != other.#data.length) return false
+
+        for (const index in this.#data) {
+            if (other.#data[index] != this.#data[index]) return false
+        }
+
+        return true
+    }
+
+    get id() {
+        return `@(${this.#data.join(',')})`
+    }
+
+    [require('util').inspect.custom](depth, options, inspect) {
+        if (depth < 0) return options.stylize('[Location]', 'special')
+        return `Location (${this.id})`
+    }
+}
+
+class Vertex {
+    #vertices = null // of LocationVertex | StateVertex | ActionVertex
+    #associations = null
+    static #hash = require('object-hash')
+
+    visit(fun, ...args) { return fun.call(this, ...args) }
+
+    get vertices() {
+        if (!this.#vertices) this.#vertices = new Array()
+        return this.#vertices
+    }
+
+    get associations() {
+        if (!this.#associations) this.#associations = new Array()
+        return this.#associations
+    }
+
+    associate(data) {
+        this.associations.push(data)
+    }
+
+    get hash() {
+        return Vertex.#hash
+    }
+
+    [require('util').inspect.custom](depth, options, inspect) {
+        if (!this.#vertices && !this.#associations) {
+            return options.stylize(`${this.constructor.name}::Empty`, 'special');
+        }
+
+
+        if (depth < 0) {
+            return options.stylize(`[${this.constructor.name}]`, 'special')
+        }
+        else {
+            const newOptions = Object.assign({}, options, {
+                depth: options.depth === null ? null : options.depth - 1,
+            });
+
+            let projection = Object.create({})
+
+            if (!!this.#vertices) {
+                for (const [key, value] of Object.entries(this.#vertices)) {
+                    projection = Object.assign(projection, { [value.id || key ]: value })
+                }
+            }
+
+            if (!!this.#associations) {
+                projection = Object.assign(projection, { ['?']: this.#associations })
+            }
+
+            return `${this.constructor.name} ${inspect(projection, newOptions)}`
+        }
+
+    }
+}
+
+class ActionVertex extends Vertex {
+    constructor(data) {
+        super(data)
+        this.action = data.action
+        this.associate({ aleph: data.aleph4action }) 
+    }
+
+    append(data) {
+        for (const index in this.vertices) {
+            const vertex = this.vertices[index]
+            if (vertex instanceof LocationVertex) {
+                if (vertex.matches(data)) {
+                    return vertex.append(data)
+                }
+            }
+            else throw { context: this, append: { data, vertex } }
+        }
+
+        const locationVertex = new LocationVertex(data)
+        this.vertices.push(locationVertex)
+        return locationVertex.append(data)
+    }
+
+    get id() {
+        return `/${this.action}/`
+    }
+
+    matches (data) {
+        return this.action == data.action
+    }
+}
+
+class StateVertex extends Vertex {
+    #state
+    constructor(data) {
+        super(data)
+        this.#state = data.state
+        this.associate({ aleph: data.aleph4state })
+    }
+
+    append(data) {
+        for (const index in this.vertices) {
+            const vertex = this.vertices[index]
+            if (vertex instanceof ActionVertex) {
+                if (vertex.matches(data)) {
+                    return vertex
+                }
+            }
+            else throw { context: this, append: { data, vertex } }
+        }
+        const actionVertex = new ActionVertex(data)
+        this.vertices.push(actionVertex)
+        return actionVertex
+    }
+
+    get id() {
+        return `#|${this.hash(this.#state, { encoding: 'base64' })}|`
+    }
+
+    matches(data) {
+        return this.hash(this.#state) === this.hash(data.state)
+    }
+}
+
+class LocationVertex extends Vertex {
+    #location
+    constructor(data) {
+        super(data)
+        this.#location = new Location(data.state.loc)
+    }
+
+    append(data) {
+        for (const index in this.vertices) {
+            const vertex = this.vertices[index]
+            if (vertex instanceof StateVertex) {
+                if (vertex.matches(data)) {
+                    return vertex.append(data)
+                }
+            }
+            else throw { context: this, append: { data, vertex } }
+        }
+        const stateVertex = new StateVertex(data)
+        this.vertices.push(stateVertex)
+        return stateVertex.append(data)
+    }
+
+    get id() {
+       return this.#location.id 
+    }
+
+    matches(data) { 
+        return this.#location.equals(new Location(data.state.loc))
+    }
+}
+
+class PrefixTree extends Vertex {
+    append(data) {
+        for (const index in this.vertices) {
+            const vertex = this.vertices[index]
+            if (vertex instanceof LocationVertex) {
+                if (vertex.matches(data)) {
+                    return vertex.append(data)
+                }
+            }
+            else throw { context: this, append: { data, vertex } }
+        }
+
+        const locationVertex = new LocationVertex(data)
+        this.vertices.push(locationVertex)
+        return locationVertex.append(data)
+    }
+}
+
+class TrajectoriesDistributionForwardDiagramVisitor {
+    static visit (root, trajectory, parameters) {
+        const [data, ...reminder] = trajectory
+        const vertex = root.append(data)
+        if (reminder.length > 0) {
+            vertex.visit(TrajectoriesDistributionForwardDiagramVisitor.visit,
+                vertex, reminder, parameters
+            ) 
+        }
+
+        vertex.associate({ P: parameters.prob } )
+    }
+}
 // TODO: how to move the functions to other js files and still make them available in webppl code??? 
 
 var locActionData2ASCIIdefaultFormat = function (x) {
@@ -182,6 +393,25 @@ module.exports = {
             result.push(res);
         }
         return result;
+    },
+
+    debug: {
+        inspect: function(...args) {
+            const util = require('util')
+            return util.inspect(...args)
+        },
+        trajectoriesDistribution: {
+            diagrams: {
+                forward: function (data) {
+                    const distribution = data.getDist()
+                    const result = new PrefixTree()
+                    for(var key in distribution) {
+                        TrajectoriesDistributionForwardDiagramVisitor.visit(result, JSON.parse(key), distribution[key])
+                    }
+                    return result
+                }
+            }
+        }
     },
 
     // TO BE MOVED TO src/utils/metalog.js:
